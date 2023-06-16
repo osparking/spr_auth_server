415eeb56142131c3539fdbcfb6a0c452807c74c6
osparking <jbpark03@gmail.com>
2023-06-16 15:17:52 +0900

    a simple security config' class

---
+++ src/main/java/space/bumtiger/auth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package space.bumtiger.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import space.bumtiger.auth.users.UserRepository;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+	// @formatter:off
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(
+			HttpSecurity http) throws Exception {
+		return http.authorizeHttpRequests(
+						requests -> requests.anyRequest().authenticated())
+				.formLogin(Customizer.withDefaults()).build();
+	}
+	// @formatter:on
+	
+	@Bean
+	UserDetailsService userDetailsService(UserRepository userRepo) {
+		return username -> userRepo.findByUsername(username);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
