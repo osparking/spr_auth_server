773e15bc68da2bcdf8a8b8c57eaffc77dc6a4dc2
osparking <jbpark03@gmail.com>
2023-06-16 15:19:27 +0900

    main class package renamed

--- src/main/java/space/bumtiger/auth/SprAuthServerApplication.java
+++ src/main/java/space/bumtiger/auth/SprAuthServerApplication.java
@@ -1,4 +1,4 @@
-package space.bumtiger.rest;
+package space.bumtiger.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
