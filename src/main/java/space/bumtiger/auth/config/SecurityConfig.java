73fe5dab49ef4278cb0c8c0acad67007ee43c8f9
osparking <jbpark03@gmail.com>
2023-06-25 15:35:44 +0900

    missing annotation prepended

--- src/main/java/space/bumtiger/auth/config/SecurityConfig.java
+++ src/main/java/space/bumtiger/auth/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package space.bumtiger.auth.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import space.bumtiger.auth.users.UserRepository;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
